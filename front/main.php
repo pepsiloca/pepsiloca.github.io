71a4ab06f73916c6c9e674659b7a5a58d896499a
Loki <summer10920@gmail.com>
2020-06-11 16:06:38 +0800

    完成首頁的最新消息顯示及more判斷

--- front/main.php
+++ front/main.php
@@ -1,7 +1,6 @@
 <div class="di"
     style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
-    <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-    </marquee>
+    <?php include "maquree.php";?>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
     
@@ -42,11 +41,31 @@
     <div
         style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+                $news=new DB("news");
+                $chk=$news->count(['sh'=>1]);
+                if($chk>=5){
+                    echo "<a href='index.php?do=news' style='float:right'>More...</a>";
+                }
+
+            ?>
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
+            <?php
+                
+                $ns=$news->all(['sh'=>1]," limit 5");
+                foreach($ns as $n){
+            ?>
+            <li><?=mb_substr($n['text'],0,20,'utf8');?>...
+                <div class='all' style="display:none"><?=$n['text'];?></div>
+            </li> 
+            <?php
+            }
+            ?>
         </ul>
         <div id="altt"
             style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+        
         </div>
         <script>
         $(".ssaa li").hover(
