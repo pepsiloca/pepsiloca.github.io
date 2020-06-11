0853ab5a46ca1d5e42da1d79bb71bf43a9b0a034
Loki <summer10920@gmail.com>
2020-06-11 16:37:53 +0800

    完成前台更多最新消息顯示及分頁

--- front/news.php
+++ front/news.php
@@ -4,9 +4,51 @@
                 <?php include "maquree.php";?>
                 <div style="height:32px; display:block;"></div>
                 <!--正中央-->
+                <?php
+
+                $news=new DB("news");
+                $total=$news->count(['sh'=>1]);
+                $num=5;
+                $pages=ceil($total/$num);
+                $now=(!empty($_GET['p']))?$_GET['p']:1;
+                $start=($now-1)*$num;
+                $ns=$news->all(['sh'=>1]," limit $start,$num");
+                ?>
+                <ol class="ssaa" start="<?=$start+1;?>">
+            <?php
+                foreach($ns as $n){
+            ?>
+            <li><?=mb_substr($n['text'],0,20,'utf8');?>...
+                <div class='all' style="display:none"><?=$n['text'];?></div>
+            </li> 
+            <?php
+            }
+            ?>
+        </ul>
                 <div style="text-align:center;">
-                    <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-                    <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
+                    <?php
+                    if(($now-1)>0){
+                    ?>
+                        <a class="bl" style="font-size:30px;" href="?do=news&p=<?=($now-1);?>">&lt;&nbsp;</a>
+                    <?php
+                    }
+                    ?>
+
+                    <?php
+                    for($i=1;$i<=$pages;$i++){
+                        $fontsize=($i==$now)?'30px':'24px';
+                    ?>
+                    <a class="bl" style="font-size:<?=$fontsize;?>;" href="?do=news&p=<?=$i;?>"><?=$i;?></a>
+                    <?php
+                    }
+                    ?>
+                                        <?php
+                    if(($now+1)<=$pages){
+                    ?>
+                    <a class="bl" style="font-size:30px;" href="?do=news&p=<?=($now+1);?>">&nbsp;&gt;</a>
+                    <?php
+                    }
+                    ?>
                 </div>
             </div>
             <div id="alt"
